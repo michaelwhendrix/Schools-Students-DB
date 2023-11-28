d6b26c32031784c15810979171657a7d924a7406
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-28 15:33:25 -0600

    added drop tables and create tables

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,40 @@
 const client = require('./client.js');
 
+const createTables = async() => {
+    try {
+        await client.query(`
+            CREATE TABLE schools(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                city VARCHAR(40),
+                state CHAR(2),
+                zip_code CHAR(5),
+                is_accredited BOOLEAN
+            );
+        `)
+
+
+    } catch (error) {
+        
+    }
+}
+
+const dropTables = async() => {
+    try {
+        await client.query(`DROP TABLE IF EXISTS schools;`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const syncAndSeed = async () =>{
     try{
         await client.connect();
         console.log('CONNECTED');
-
+        await dropTables();
+        console.log('DROP TABLES');
+        await createTables();
+        console.log('CREATE TABLES')
 
         client.end();
     }catch(err){console.log(err)};
